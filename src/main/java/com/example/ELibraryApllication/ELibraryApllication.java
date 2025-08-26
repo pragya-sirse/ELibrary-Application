5536cfbe9236e5df48a4bf4b627c2fd1db9b546b
pragya <pragyasirse@gmail.com>
2025-08-26 21:41:08 +0530

    DB config

--- src/main/java/com/example/ELibraryApllication/ELibraryApllication.java
+++ src/main/java/com/example/ELibraryApllication/ELibraryApllication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ELibraryApllicationApplication {
+public class ELibraryApllication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ELibraryApllicationApplication.class, args);
+		SpringApplication.run(ELibraryApllication.class, args);
 	}
 
 }
--- src/main/java/com/example/ELibraryApllication/config/CorsConfig.java
+++ src/main/java/com/example/ELibraryApllication/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.ELibraryApllication.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
